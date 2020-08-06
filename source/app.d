68091be4c73609bad475f2b10bebeef783335ead
SealabJaster <sealabjaster@gmail.com>
2020-08-06 07:31:06 +0100

    New Start

--- source/app.d
+++ source/app.d
@@ -1,6 +1,5 @@
 import std.stdio, std.experimental.logger;
 import bindbc.sdl, erupted;
-import game.graphics.window, game.graphics.sdl, game.graphics.vulkan, game.graphics.renderer;
 
 void main()
 {
@@ -18,49 +17,4 @@ void main()
         if(goBack)
             chdir("..");
     }
-
-    SDL.onInit();
-    Window.onInit();
-    Vulkan.onInit();
-
-    // Temporary loop
-    auto renderer = new Renderer();
-    bool loop = true;
-    uint lastTicks = 0;
-    uint ticks = 0;
-    uint frame = 0;
-    while(loop)
-    {
-        SDL_Event event;
-        Window.nextEvent(&event);
-
-        if(event.type == SDL_QUIT)
-            loop = false;
-
-        if(event.window.event == SDL_WINDOWEVENT_MINIMIZED)
-        {
-            while(event.window.event != SDL_WINDOWEVENT_RESTORED)
-                Window.nextEvent(&event);
-        }
-
-        RendererResources.pushConstants.ticks = SDL_GetTicks();
-        renderer.startFrame();
-        renderer.endFrame();
-
-        frame++;
-        ticks += SDL_GetTicks() - lastTicks;
-        if(ticks >= 1000)
-        {
-            info("FPS: ", frame);
-            ticks -= 1000;
-            frame = 0;
-        }
-
-        lastTicks = SDL_GetTicks();
-    }
-
-    Vulkan.waitUntilAllDevicesAreIdle();
-    Vulkan.onUninit();
-    Window.onUninit();
-    SDL.onUninit();
 }
