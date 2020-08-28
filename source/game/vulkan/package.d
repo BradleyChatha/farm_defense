4e30b8463cbb5b58c843106cd4311e0d2751664f
SealabJaster <sealabjaster@gmail.com>
2020-08-28 05:59:32 +0100

    asd

--- source/game/vulkan/package.d
+++ source/game/vulkan/package.d
@@ -1,7 +1,7 @@
 module game.vulkan;
 
 public import erupted;
-public import game.vulkan._events : OnFrameChangeId; // For... some reason, types can't see this without an explicit import.
+public import game.vulkan._events : OnFrameChangeId, OnSwapchainRecreateId; // For... some reason, types can't see these without an explicit import.
 public import game.vulkan.common, game.vulkan.globals, game.vulkan.device, game.vulkan.surface, game.vulkan.instance,
               game.vulkan.queue, game.vulkan.tracker, game.vulkan.swapchain, game.vulkan.image, game.vulkan.command,
               game.vulkan.shader, game.vulkan.pipeline, game.vulkan._events;
